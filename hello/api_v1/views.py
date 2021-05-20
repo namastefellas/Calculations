5f3a60d66827fcf630aee9a3f4a85468a9ba4a62
German <coolmatoff@gmail.com>
2021-05-20 20:31:40 +0600

    added functions to index

--- hello/api_v1/views.py
+++ hello/api_v1/views.py
@@ -1,11 +1,10 @@
-from django.http.response import HttpResponse, JsonResponse
 import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
-def add(request):
+def add(request, *args, **kwagrs):
     num = request.body
     if num:
         nums = json.loads(num)
@@ -18,11 +17,13 @@ def add(request):
             }
             response = JsonResponse(response_data)
         except:
-            respnse_data = {
+            response_data = {
                 'result': 'Enter whole numbers'
             }
             response = JsonResponse(response_data)
             response.status_code = 400
+
+    return response
 
 def subtract(request):
     num = request.body
@@ -37,11 +38,14 @@ def subtract(request):
             }
             response = JsonResponse(response_data)
         except:
-            respnse_data = {
+            response_data = {
                 'result': 'Enter whole numbers'
             }
             response = JsonResponse(response_data)
             response.status_code = 400
+
+    return response
+    
 
 def multiply(request):
     num = request.body
@@ -56,11 +60,13 @@ def multiply(request):
             }
             response = JsonResponse(response_data)
         except:
-            respnse_data = {
+            response_data = {
                 'result': 'Enter whole numbers'
             }
             response = JsonResponse(response_data)
             response.status_code = 400
+
+    return response
 
 def divide(request):
     num = request.body
@@ -69,16 +75,25 @@ def divide(request):
         try:
             nums['A'] = int(nums['A'])
             nums['B'] = int(nums['B'])
-            total = nums['A'] // nums['B']
-            response_data = {
-                'result': str(total)
-            }
-            response = JsonResponse(response_data)
+            if nums['B'] == 0:
+                response_data = {
+                    'result': 'Divided by zero!'
+                }
+                response = JsonResponse(response_data)
+                response.status_code = 400
+            else:
+                total = nums['A'] // nums['B']
+                response_data = {
+                    'result': str(total)
+                }
+                response = JsonResponse(response_data)
         except:
-            respnse_data = {
+            response_data = {
                 'result': 'Enter whole numbers'
             }
             response = JsonResponse(response_data)
             response.status_code = 400
+
+    return response
 
 
