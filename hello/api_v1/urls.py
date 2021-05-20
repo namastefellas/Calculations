61b934f0e14a6be91944d4a5a34463ece4001e1b
German <coolmatoff@gmail.com>
2021-05-20 17:57:56 +0600

    added math methods

--- hello/api_v1/urls.py
+++ hello/api_v1/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from api_v1.views import my_first_api_view
+from api_v1.views import add, subtract, multiply, divide 
 
 app_name = 'api_v1'
 
 urlpatterns = [
-    path('test/', my_first_api_view, name='test_api')
+    path('add/', add, name='test_add'),
+    path('subtract/', subtract, name='test_subtract'),
+    path('multiply/', multiply, name='test_multiply'),
+    path('divide/', divide, name='test_divide')
 ] 
--- hello/api_v1/views.py
+++ hello/api_v1/views.py
@@ -1,9 +1,66 @@
 from django.http.response import HttpResponse
+import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
 # Create your views here.
 
-def my_first_api_view(request):
-    response = HttpResponse()
+def add(request):
+    num = json.loads(request.body)
+    if num['A'] and num['B']:
+        total = num['A'] + num['B']
+        response_data = {
+            'result': str(total),
+            'method': request.method
+        }
+
+    response = HttpResponse(json.dumps(response_data))
     response['Content-Type'] = 'application/json'
+    return response
+
+def subtract(request):
+    num = json.loads(request.body)
+    if num['A'] and num['B']:
+        total = num['A'] - num['B']
+        response_data = {
+            'result': str(total),
+            'method': request.method
+        }
+
+    response = HttpResponse(json.dumps(response_data))
+    response['Content-Type'] = 'application/json'
+    return response
+
+def multiply(request):
+    num = json.loads(request.body)
+    if num['A'] and num['B']:
+        total = num['A'] * num['B']
+        response_data = {
+            'result': str(total),
+            'method': request.method
+        }
+    
+
+    response = HttpResponse(json.dumps(response_data))
+    response['Content-Type'] = 'application/json'
+    return response
+
+def divide(request):
+    try:
+        num = json.loads(request.body)
+        if num['A'] and num['B']:
+            total = num['A'] // num['B']
+            response_data = {
+                'result': str(total),
+                'method': request.method
+            }
+    except:
+        if num['A'] or num['B'] == 0:
+            response_data = {
+                'result': 'Division by zero!',
+                'method': request.method
+            }
+
+    response = HttpResponse(json.dumps(response_data))
+    response['Content-Type'] = 'application/json'
+    return response
